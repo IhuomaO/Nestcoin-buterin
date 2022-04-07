26b6fbf8deefe4c854fdfc442327f93b65efa862
Samuel Osinloye <psalmueloye@gmail.com>
2022-04-08 00:45:38 +0100

    fix ft-rewards-card

--- packages/react-app/src/Sections/CouponSection.jsx
+++ packages/react-app/src/Sections/CouponSection.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Card from "../components/Card";
 import { CardInformation } from "../data";
-import Helpers from "../helpers/contractInfo";
+
 const CouponSection = () => {
   return (
     <div className="container mx-auto flex flex-wrap items-start justify-center my-16">
       {CardInformation.map(card => (
         <Card key={card.id} {...card} />
       ))}
-      {console.log(Helpers)}
     </div>
   );
 };
