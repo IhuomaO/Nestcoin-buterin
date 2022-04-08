82caf2f2725df47b840196998f063d59e8f3e2ad
Kainy <iartkhay@gmail.com>
2022-04-08 02:45:00 +0100

    Merge branch 'front-end' with reward feature into transfer-interface

--- packages/react-app/src/Components/Button.js
+++ packages/react-app/src/Components/Button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button = ({ className, submit, children, ...props }) => {
+const Button = ({ className, children, ...props }) => {
   return (
-    <button className={`${className} flex h-10 px-5 min-w-[130px] rounded-full active:bg-green-600 transition-colors items-center bg-green-700 justify-center text-xl mx-auto text-green-50`} submit={submit ? 'submit' : ''} {...props} >
+    <button className={`${className} my-3 h-10 min-w-[130px] text-lg font-semibold text-indigo-500 px-4 py-3 bg-gray-300 rounded-full hover:bg-indigo-500 active:bg-indigo-400 hover:text-white transition duration-300 inline-flex items-center justify-center leading-snug`} {...props} >
       {children}
     </button >
   )
 }
 
-export default Button
+  export default Button
--- packages/react-app/src/Views/Dashboard.js
+++ packages/react-app/src/Views/Dashboard.js
@@ -1,48 +1,21 @@
-import React, { useState } from 'react'
-import Web3 from 'web3'
-import Button from '../Components/Button';
-import Input from '../Components/Input'
+import React from 'react'
+import CouponSection from "../Sections/CouponSection";
+import TransferToken from '../Sections/TransferToken';
 
 const Dashboard = () => {
-  const [sendAddress, setSendAddress] = useState('')
 
-  const isValidAddress = (address) => {
-    try {
-      const web3 = new Web3()
-      web3.utils.toChecksumAddress(address)
-      return true
-    } catch (error) {
-      return false
-    }
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isValidAddress(sendAddress)) return
-
-  }
 
   return (
-    <div className='md:p-10 space-y-5'>
+    <div className='md:p-10 space-y-20'>
       <div className='w-full text-center text-4xl uppercase py-20'>
         Balance heading
       </div>
-      <form id='transfer' className='w-[550px] lg:w-[800px] py-10 rounded-md bg-white shadow-md mx-auto flex flex-col border text-center space-y-5' onSubmit={handleSubmit}>
-        <h3 className='text-4xl uppercase font-semibold py-5'>Transfer Token</h3>
 
-        <Input label={'Address'} name='send-address' onChange={(e) => setSendAddress(() => e.target.value)} value={sendAddress} />
-        <Input label={'Amount'} name='Amount' placeholder='Amount to send in BTU' />
-        {
-          sendAddress.length > 10 && !isValidAddress(sendAddress) &&
-          <div className='text-red-700'> Not a valid address </div>
-        }
+      <TransferToken />
+      <CouponSection />
 
-        <div className='w-full'>
-          <Button submit form="transfer" value="Submit">Send</Button>
-        </div>
-      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
