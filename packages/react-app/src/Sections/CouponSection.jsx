c35244e215fd0cd9e3386c73ef7e54dd688518ef
Kainy <iartkhay@gmail.com>
2022-04-08 01:49:41 +0100

    fixed merge bugs

--- packages/react-app/src/Sections/CouponSection.jsx
+++ packages/react-app/src/Sections/CouponSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../components/Card";
+import Card from "../Components/Card";
 import { CardInformation } from "../data";
 
 const CouponSection = () => {
--- packages/react-app/src/components/Card.jsx
+++ packages/react-app/src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Card = ({ id, bgImage, body, value, name }) => {
   return (
     <div className="lg:w-1/3 w-full mb-6 lg:p-3">
-      <div className="bg-gray-200 rounded-xl">
-        <img src={bgImage} className="max-w-full h-auto" alt={bgImage} />
+      <div className="bg-gray-200 rounded-xl  h-[500px]">
+        <img src={bgImage} className="w-full h-60" alt={bgImage} />
         <div className="px-6 pb-6 pt-3">
           <h2 className="text-xl font-bold mb-2 uppercase">{name}</h2>
           <p className="text-gray-600 leading-5 mb-4">{body}</p>
